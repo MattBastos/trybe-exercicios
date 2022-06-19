caa8b37cd8046803e3eadd411eacbba3a26fe1b3
Matthews Bastos <matthewsbasttos@gmail.com>
2022-06-19 13:33:18 -0300

    Adiciona restante do exercicio 2

--- modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicio-03/script.js
+++ modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicio-03/script.js
@@ -1,8 +1,14 @@
 const verifyInputs = (value1, value2) => {
   if (!value1 || !value2) {
-    throw new Error('Preencha os campos obrigatórios!');
+    throw new Error('Informe números nos campos obrigatórios!');
+  }
+
+  if (isNaN(value1) || isNaN(value2)) {
+    throw new Error('Informe apenas números nos campos obrigatórios!');
   }
 };
+
+
 
 const sum = () => {
   try {
@@ -11,10 +17,9 @@ const sum = () => {
     verifyInputs(value1, value2);
     const result = parseInt(value1) + parseInt(value2);
     document.getElementById('result').innerText = `Resultado: ${result}`;
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
   } catch (error) {
     document.getElementById('result').innerText = `Erro: ${error.message}`;
+  } finally {
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
   }
