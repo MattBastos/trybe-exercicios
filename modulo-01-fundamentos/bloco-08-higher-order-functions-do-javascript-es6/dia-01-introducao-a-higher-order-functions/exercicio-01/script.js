e42c3d99456249170a997171fa466f01643878d8
Matthews Bastos <matthewsbasttos@gmail.com>
2022-06-22 16:13:32 -0300

    Adiciona exercicio 1

---
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-01/script.js
@@ -0,0 +1,9 @@
+const wakeUp = () => 'Acordando!';
+
+const coffeeTime = () => 'Bora tomar café!';
+
+const sleep = () => 'Partiu dormir!';
+
+const doingThings = (callback) => console.log(callback);
+
+doingThings(coffeeTime());
