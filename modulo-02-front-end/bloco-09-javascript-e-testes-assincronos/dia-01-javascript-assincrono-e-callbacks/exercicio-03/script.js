97afbf7426d9a6b2784a89ef99dd9aaffbc1f150
Matthews Bastos <matthewsbasttos@gmail.com>
2022-07-09 11:31:27 -0300

    Adiciona exercicios

---
+++ modulo-02-front-end/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio-03/script.js
@@ -0,0 +1,27 @@
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (successCallback, errorCallback) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      successCallback(country);
+    } else {
+      const errorMessage = "Country could not be found";
+      errorCallback(errorMessage);
+    }
+  }, delay());
+};
+
+getCountry(countryName, printErrorMessage);
+
+getCountry(countryCurrency, printErrorMessage);
